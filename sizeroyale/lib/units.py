56d5d9844ebb31be3357183b38785c365bcec133
DigiDuncan <digiduncan@gmail.com>
2020-11-11 19:44:36 -0500

    genericize units

--- sizeroyale/lib/units.py
+++ sizeroyale/lib/units.py
@@ -2,20 +2,22 @@ import requests
 from decimal import Decimal
 from urllib.parse import quote
 
-class SV:
-    def __init__(self):
-        pass
 
-    @classmethod
-    def parse(cls, s):
-        return _parse("SV", s)
+class UnitWrapper:
+    def __init__(self, unit):
+        self._unit = unit
+
+    def parse(self, s):
+        t = self._unit
+        if t not in ["SV", "WV", "TV", "Diff", "Rate", "LimitedRate"]:
+            raise ValueError(f"Parsing type {t} not valid.")
+        r = requests.get(f"https://nizebot.bew.by/unit/{t}/parse?s=" + quote(s))
+        responsejson = r.json()
+        if t in ["SV", "WV", "TV"]:
+            return Decimal(responsejson[t])
+        return responsejson[t]
 
 
-def _parse(t, s):
-    if t not in ["SV", "WV", "TV", "Diff", "Rate", "LimitedRate"]:
-        raise ValueError(f"Parsing type {t} not valid.")
-    r = requests.get(f"https://nizebot.bew.by/unit/{t}/parse?s=" + quote(s))
-    responsejson = r.json()
-    if t in ["SV", "WV", "TV"]:
-        return Decimal(responsejson[t])
-    return responsejson[t]
+SV = UnitWrapper("SV")
+WV = UnitWrapper("WV")
+TV = UnitWrapper("TV")
