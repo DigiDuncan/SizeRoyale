84c0d8d3d5cf4954297c242aaafe24e040acbcd4
DigiDuncan <digiduncan@gmail.com>
2020-11-16 22:47:48 -0500

    log errors!

--- sizeroyale/lib/classes/game.py
+++ sizeroyale/lib/classes/game.py
@@ -1,9 +1,16 @@
+import logging
+
 from sizeroyale.lib.classes.royale import Royale
+
+logger = logging.getLogger("sizeroyale")
 
 
 class Game:
     def __init__(self, filepath):
         self.royale = Royale(filepath)
+        if self.royale.parser.errors:
+            for e in self.royale.parser.errors:
+                logger.error(e)
 
     def next_round(self):
         raise NotImplementedError
